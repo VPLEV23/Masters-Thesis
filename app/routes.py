66c039f4c00944c2ce8d65dfc9a440a89ceaf78d
Vlad Bulhakov <bladetheblade12@gmail.com>
2023-09-26 20:09:08 +0200

    login functional done

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
-# app.py
 from flask import render_template, url_for, flash, redirect
 from app import app, db, login_manager
 from app.forms import RegistrationForm
 from app.models import User
 from .models import User
+from flask_login import login_user, current_user
+from app.forms import LoginForm
+from flask_login import logout_user
 @app.route('/')
 @app.route('/index')
 def index():
@@ -25,7 +27,24 @@ def register():
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-@app.route("/login")
-def login():
-    return "Login Page"
 
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))  # or another route if needed
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember.data)
+            # Redirect to the desired page after login
+            return redirect(url_for('index'))  
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
